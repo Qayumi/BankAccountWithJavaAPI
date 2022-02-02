75a0ddbb962ee8789e1567c2a032fb07d9cace5d
Qayumi <qayumee@gmail.com>
2022-02-02 22:07:29 +0200

    The first Version of BankAccount project

---
+++ src/main/java/ee/swedb/bankaccountwithjavaapi/BankAccountWithJavaApiApplication.java
@@ -0,0 +1,13 @@
+package ee.swedb.bankaccountwithjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountWithJavaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountWithJavaApiApplication.class, args);
+    }
+
+}
